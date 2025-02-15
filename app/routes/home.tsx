d070ae2cbac8f4154534dd1f3b28646579772d9a
Ofonna Nweze <nwezeofonna@gmail.com>
2025-02-14 20:01:55 -0600

    remove unused meta function from home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,6 @@
 import type { Route } from "./+types/home";
 import { Box } from "@mui/material";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
 
 export default function Home() {
   return <Box>
