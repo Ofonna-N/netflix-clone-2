10b81459a42baf7114ee16612c6cd3ccba12a95d
Ofonna Nweze <nwezeofonna@gmail.com>
2025-02-15 12:08:18 -0600

    enhance theme configuration with responsive font sizes and additional typography settings

--- app/context/mui_theme_providers.tsx
+++ app/context/mui_theme_providers.tsx
@@ -1,13 +1,29 @@
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import {
+  createTheme,
+  CssBaseline,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material";
 
-const appTheme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#e50914",
+const appTheme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#e50914",
+      },
+      secondary: {
+        main: "#ffffff",
+      },
     },
-  },
-});
+    typography: {
+      h1: {
+        fontSize: "3.375rem", // 54px to rem
+        fontWeight: 700, // bold
+      },
+    },
+  })
+);
 
 export default function MuiThemeProviders({
   children,
