11003edfa7ffaafca56e685714e8b05127b29c4e
Ofonna Nweze <nwezeofonna@gmail.com>
2025-02-20 13:38:54 -0600

    add BackgroundImage component to Landing for improved visual presentation

--- app/features/landing/components/landing.tsx
+++ app/features/landing/components/landing.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import Header from "./header";
 import Hero from "./hero";
+import BackgroundImage from "./background_image";
 
 export default function Landing() {
   return (
@@ -36,23 +37,17 @@ export default function Landing() {
           },
         }}
       >
-        <Box
-          component={"img"}
-          src={
-            "https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/US-en-20250210-TRIFECTA-perspective_5cc3f8aa-0c98-40f5-96f7-ac7e4af57d4a_medium.jpg"
-          }
-          alt="hero image"
-          sx={{
-            width: "100%",
-            height: "100%",
-            position: "absolute",
-            objectFit: "cover",
-            objectPosition: "center",
-            zIndex: 1,
-          }}
-        />
+        <BackgroundImage />
         <Header />
         <Hero />
+      </Box>
+      <Box
+        sx={{
+          height: "200px",
+          backgroundColor: "black",
+        }}
+      >
+        Content
       </Box>
     </Box>
   );
