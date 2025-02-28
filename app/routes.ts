c0574c8edeed3ead00bfd6236bf8346c8b577bd1
Ofonna Nweze <nwezeofonna@gmail.com>
2025-02-28 09:02:10 -0600

    add layout for browse routes and update routing configuration

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,15 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("login", "routes/login.tsx"),
+  layout("routes/browse/layout.tsx", [
+    index("routes/browse/select_account.tsx"),
+    route("browse", "routes/browse/browse.tsx"),
+  ]),
 ] satisfies RouteConfig;
