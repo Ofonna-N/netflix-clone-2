f22705cba1186817998aedb4fc756076e72ec3a0
Ofonna Nweze <nwezeofonna@gmail.com>
2025-03-04 14:10:10 -0600

    implement responsive navigation with dropdown menu for smaller screens

--- app/routes/browse/browse.tsx
+++ app/routes/browse/browse.tsx
@@ -1,26 +1,60 @@
-import { AppBar, Box, Container, Link, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Container,
+  Link,
+  Toolbar,
+  useMediaQuery,
+} from "@mui/material";
 import Logo from "~/components/logo";
-
-const browseNavLinks = [
-  { label: "Home", path: "/" },
-  { label: "TV Shows", path: "/tv-shows" },
-  { label: "Movies", path: "/movies" },
-  { label: "New & Popular", path: "/new-and-popular" },
-  { label: "My List", path: "/my-list" },
-  { label: "Browse by Languages", path: "/browse-by-languages" },
-];
+import browseNavLinks from "~/features/browse/constants/browse_navlinks";
+import DropdownMenu from "~/features/browse/components/dropdown_menu";
 
 export default function Browse() {
+  const isMinMd = useMediaQuery((theme) => theme.breakpoints.up("md"));
+
   return (
-    <Box>
+    <Box
+      sx={{
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          backgroundColor: "royalblue",
+          minHeight: " 400px",
+          position: "absolute",
+          top: "0",
+          left: "0",
+          aspectRatio: "16/9",
+          "::before": {
+            content: "''",
+            display: "block",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            backgroundImage:
+              "linear-gradient(to bottom, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 20%,rgba(0,0,0,0) 100%)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            opacity: 0.5,
+            zIndex: 999,
+          },
+        }}
+      ></Box>
       <AppBar position="static" color="transparent">
         <Container maxWidth="xl">
           <Toolbar>
             <Box
               sx={{
+                position: "relative",
                 display: "flex",
                 gap: 3,
                 alignItems: "center",
+                zIndex: 1000,
               }}
             >
               <Logo
@@ -33,24 +67,33 @@ export default function Browse() {
                   },
                 }}
               />
-              <Box
-                sx={{
-                  display: "flex",
-                  gap: 2,
-                }}
-              >
-                {browseNavLinks.map((navLink) => (
-                  <Link
-                    key={navLink.label}
-                    color="secondary"
-                    href="#"
-                    underline="none"
-                    fontSize={"0.9rem"}
-                  >
-                    {navLink.label}
-                  </Link>
-                ))}
-              </Box>
+              {!isMinMd ? (
+                <DropdownMenu />
+              ) : (
+                <Box
+                  sx={{
+                    display: "flex",
+                    gap: 2,
+                  }}
+                >
+                  {browseNavLinks.map((navLink) => (
+                    <Link
+                      key={navLink.label}
+                      color="secondary"
+                      href="#"
+                      underline="none"
+                      fontSize={"0.9rem"}
+                      sx={{
+                        ":hover": {
+                          opacity: 0.7,
+                        },
+                      }}
+                    >
+                      {navLink.label}
+                    </Link>
+                  ))}
+                </Box>
+              )}
             </Box>
           </Toolbar>
         </Container>
