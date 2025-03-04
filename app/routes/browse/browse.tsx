597b71be8489b93204976916f4674bfa5518ce87
Ofonna Nweze <nwezeofonna@gmail.com>
2025-03-04 09:02:32 -0600

    implement navigation bar in Browse component with links to various sections

--- app/routes/browse/browse.tsx
+++ app/routes/browse/browse.tsx
@@ -1,3 +1,60 @@
+import { AppBar, Box, Container, Link, Toolbar } from "@mui/material";
+import Logo from "~/components/logo";
+
+const browseNavLinks = [
+  { label: "Home", path: "/" },
+  { label: "TV Shows", path: "/tv-shows" },
+  { label: "Movies", path: "/movies" },
+  { label: "New & Popular", path: "/new-and-popular" },
+  { label: "My List", path: "/my-list" },
+  { label: "Browse by Languages", path: "/browse-by-languages" },
+];
+
 export default function Browse() {
-  return <div>B</div>;
+  return (
+    <Box>
+      <AppBar position="static" color="transparent">
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 3,
+                alignItems: "center",
+              }}
+            >
+              <Logo
+                logoProps={{
+                  sx: {
+                    width: {
+                      xs: "100px",
+                      sm: "150px",
+                    },
+                  },
+                }}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  gap: 2,
+                }}
+              >
+                {browseNavLinks.map((navLink) => (
+                  <Link
+                    key={navLink.label}
+                    color="secondary"
+                    href="#"
+                    underline="none"
+                    fontSize={"0.9rem"}
+                  >
+                    {navLink.label}
+                  </Link>
+                ))}
+              </Box>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
 }
