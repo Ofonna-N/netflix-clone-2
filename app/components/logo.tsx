1573d29d0c21764950b66a6861c0e47225449eac
Ofonna Nweze <nwezeofonna@gmail.com>
2025-03-04 09:02:25 -0600

    enhance Logo component to accept custom BoxProps for flexible styling

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,19 +1,27 @@
-import { Box } from "@mui/material";
+import { Box, type BoxProps } from "@mui/material";
 import LogoSvg from "~/assets/flixclonev1.svg";
 
-export default function Logo() {
+type Props = {
+  logoProps?: BoxProps;
+};
+export default function Logo(props: Props) {
+  const { sx, ...otherBoxProps } = props.logoProps ?? {};
   return (
     <Box
+      {...otherBoxProps}
       component={"img"}
       src={LogoSvg}
       alt="Logo"
       sx={{
-        width: {
-          xs: "100px",
-          sm: "150px",
-          md: "180px",
-          lg: "200px",
-        },
+        ...sx,
+        width: (sx as any)?.width
+          ? { ...((sx as any)?.width as any) }
+          : {
+              xs: "100px",
+              sm: "150px",
+              md: "180px",
+              lg: "200px",
+            },
         transform: "translateY(5px)",
       }}
     />
