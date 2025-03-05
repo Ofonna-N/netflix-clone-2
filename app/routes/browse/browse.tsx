1ea7513231e195072b2d60899fc7a8fdfb3ad869
Ofonna Nweze <nwezeofonna@gmail.com>
2025-03-05 10:59:40 -0600

    re organise browse componsition

--- app/routes/browse/browse.tsx
+++ app/routes/browse/browse.tsx
@@ -19,33 +19,7 @@ export default function Browse() {
         position: "relative",
       }}
     >
-      <Box
-        sx={{
-          width: "100%",
-          backgroundColor: "royalblue",
-          minHeight: " 400px",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          aspectRatio: "16/9",
-          "::before": {
-            content: "''",
-            display: "block",
-            position: "absolute",
-            top: "0",
-            left: "0",
-            width: "100%",
-            height: "100%",
-            backgroundImage:
-              "linear-gradient(to bottom, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 20%,rgba(0,0,0,0) 100%)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            opacity: 0.5,
-            zIndex: 999,
-          },
-        }}
-      ></Box>
-      <AppBar position="static" color="transparent">
+      <AppBar position="fixed" color="transparent">
         <Container maxWidth="xl">
           <Toolbar>
             <Box
@@ -100,6 +74,34 @@ export default function Browse() {
           </Toolbar>
         </Container>
       </AppBar>
+      <Box
+        sx={{
+          width: "100%",
+          backgroundColor: "royalblue",
+          minHeight: "400px",
+          maxHeight: "800px",
+          position: "relative",
+          top: "0",
+          left: "0",
+          aspectRatio: "16/9",
+          "::before": {
+            content: "''",
+            display: "block",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "400px",
+            backgroundImage:
+              "linear-gradient(to bottom, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 20%,rgba(0,0,0,0) 100%)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            opacity: 0.5,
+            zIndex: 999,
+          },
+        }}
+      ></Box>
+      <Box sx={{ bgcolor: "wheat" }}>Test section</Box>
     </Box>
   );
 }
