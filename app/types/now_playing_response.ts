8333a427c781923e11c363575c9e60fd7dede0be
Ofonna Nweze <nwezeofonna@gmail.com>
2025-03-05 14:37:34 -0600

    add Movie and NowPlayingMoviesResponse types for improved type safety

---
+++ app/types/now_playing_response.ts
@@ -0,0 +1,12 @@
+import type { Movie } from "./movie";
+
+export type NowPlayingMoviesResponse = {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+};
