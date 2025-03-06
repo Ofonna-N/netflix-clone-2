42d2cfae2e382c1335d278d4efd813afd46de163
Ofonna Nweze <nwezeofonna@gmail.com>
2025-03-06 16:40:18 -0600

    refactor Browse component by removing AppBar and related navigation elements for a simplified layout

--- app/routes/browse/browse.tsx
+++ app/routes/browse/browse.tsx
@@ -1,79 +1,11 @@
-import {
-  AppBar,
-  Box,
-  Container,
-  Link,
-  Toolbar,
-  useMediaQuery,
-} from "@mui/material";
-import Logo from "~/components/logo";
-import browseNavLinks from "~/features/browse/constants/browse_navlinks";
-import DropdownMenu from "~/features/browse/components/dropdown_menu";
-import { Link as RouterLink } from "react-router";
+import { Box } from "@mui/material";
 export default function Browse() {
-  const isMinMd = useMediaQuery((theme) => theme.breakpoints.up("md"));
-
   return (
     <Box
       sx={{
         position: "relative",
       }}
     >
-      <AppBar position="fixed" color="transparent" sx={{ boxShadow: "none" }}>
-        <Container maxWidth="xl">
-          <Toolbar>
-            <Box
-              sx={{
-                position: "relative",
-                display: "flex",
-                gap: 3,
-                alignItems: "center",
-                zIndex: 1000,
-              }}
-            >
-              <Link component={RouterLink} to="/browse">
-                <Logo
-                  logoProps={{
-                    sx: {
-                      width: {
-                        xs: "100px",
-                        sm: "150px",
-                      },
-                    },
-                  }}
-                />
-              </Link>
-              {!isMinMd ? (
-                <DropdownMenu />
-              ) : (
-                <Box
-                  sx={{
-                    display: "flex",
-                    gap: 2,
-                  }}
-                >
-                  {browseNavLinks.map((navLink) => (
-                    <Link
-                      key={navLink.label}
-                      color="secondary"
-                      href="#"
-                      underline="none"
-                      fontSize={"0.9rem"}
-                      sx={{
-                        ":hover": {
-                          opacity: 0.7,
-                        },
-                      }}
-                    >
-                      {navLink.label}
-                    </Link>
-                  ))}
-                </Box>
-              )}
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
       <Box
         sx={{
           width: "100%",
