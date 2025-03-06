e90ad0d1305cc05f7874f91edb9b196a96ddc464
Ofonna Nweze <nwezeofonna@gmail.com>
2025-03-06 16:40:23 -0600

    add NavBar component to BrowseLayout for improved navigation

--- app/routes/browse/browse_layout.tsx
+++ app/routes/browse/browse_layout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router";
 import SelectAccountView from "~/features/browse/components/select_account_view";
 import useAuthContext from "~/hooks/use_auth_context";
+import { Box } from "@mui/material";
+import NavBar from "~/features/browse/components/nav_bar";
 
 export default function BrowseLayout() {
   const authContext = useAuthContext();
@@ -8,5 +10,10 @@ export default function BrowseLayout() {
   if (!authContext.user) {
     return <SelectAccountView />;
   }
-  return <Outlet />;
+  return (
+    <Box>
+      <NavBar />
+      <Outlet />;
+    </Box>
+  );
 }
